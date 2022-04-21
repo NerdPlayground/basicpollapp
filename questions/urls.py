ed8c614e151572451d04c10512e46bf7a187a7e6
Paqimm <mcgeewilliam815@gmail.com>
2022-04-21 15:59:49 +0300

    chore (Namespace) : add questions urls to questions namespace

--- questions/urls.py
+++ questions/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from questions.views import all_questions,question
 
+app_name= "questions"
 urlpatterns= [
     path("all-questions/",all_questions,name="all-questions"),
     path("question/<str:pk>/",question,name="question")
