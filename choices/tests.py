19d6ffc631e33c94d8633cef03db520c998ae763
Paqimm <mcgeewilliam815@gmail.com>
2022-04-25 15:29:53 +0300

    chore (Test) : test choices app functions

--- choices/tests.py
+++ choices/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from choices.models import Choice
+from questions.models import Question
 
-# Create your tests here.
+class ChoiceModel(TestCase):
+    def test_objest_representation(self):
+        question= Question.objects.create(
+            title="Question",
+            body="This is a question"
+        )
+        choice= Choice.objects.create(
+            question=question,
+            body="This is a choice",
+        )
+
+        self.assertEquals(choice.__str__(),choice.body)
