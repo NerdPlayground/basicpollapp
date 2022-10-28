79db4aba860a769e86b40b1d820fb140fd8976ed
Paqimm <mcgeewilliam815@gmail.com>
2022-10-29 02:52:30 +0300

    chore (Project) : deploy web app

--- basicpoll/settings.py
+++ basicpoll/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production! 
 DEBUG = config('DEBUG',True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-env.eba-9vqugbwe.us-west-2.elasticbeanstalk.com']
 
 # Application definition
 
